cca9e8a5ee59e50e44086619b2fcaa2848b448a3
Chi-MinJimLiao <116545179+Chi-MinJimLiao@users.noreply.github.com>
2022-12-30 18:52:11 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+def fibonacci(n):
+    # Taking 1st two fibonacci numbers as 0 and 1
+    f=[0,1]
+
+    for i in range(2,n+1):
+        print('called funciton')
+        f.append(f[i-1]+f[i-2])
+    return f[n]
+
+
+
+
+
+
+if __name__ == '__main__':
+    n= 5
+    print(fib(n))
+    print(fibonacci(n))
